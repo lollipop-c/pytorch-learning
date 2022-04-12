b0c4a2583860842aa59de846e4ad5db9e9f4c5a4
lollipop <leonoayang@gmail.com>
2022-04-13 00:16:35 +0800

    Dataset类练习

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+import read_data
+from PIL import Image
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    root_dir = "dataset/train"
+    ants_label_dir = "ants_image"
+    bees_label_dir = "bees_image"
+    ants_dataset = read_data.MyData(root_dir, ants_label_dir)
+    bees_dataset = read_data.MyData(root_dir, bees_label_dir)
+    train_dataset = bees_dataset + ants_dataset
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    img, label = train_dataset[0]
+    img.show()

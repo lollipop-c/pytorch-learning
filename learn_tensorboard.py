b6d5be3a2f2ecd5cd86fa023f06ee869ba49ac82
Leonard <leonoayang@gmail.com>
2022-04-14 23:39:52 +0800

    Transform 学习

--- learn_tensorboard.py
+++ learn_tensorboard.py
@@ -9,7 +9,7 @@ def test_add_scalar():
     writer.close()
 
 
-def test_add_image(img,label,step):
+def test_add_image(img, label, step):
     writer = SummaryWriter("logs")
     for i in range(100):
         writer.add_image("{0}".format(label), img, step, dataformats="HWC")
